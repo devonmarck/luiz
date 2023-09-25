14256d7b0283db7ea9c28b302886ebcfe011632c
devonmarck <141735153+devonmarck@users.noreply.github.com>
2023-09-25 11:58:16 -0300

    Add files via upload

---
+++ profile/js/script.js
@@ -0,0 +1,9 @@
+function enviarMensagem(){
+
+var nome = document.getElementById("idnome");
+ if (nome.value != "") {
+
+    alert ("Obrigado, sr(a)"+ nome.value +", sua mensagem foi enviada com sucesso!");
+ }
+
+}
